3424effe13f7c071132b263dede2ff9a88e62cb6
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2019-12-22 23:18:08 +0530

    Added getFileContents in tailLib for reading file

--- src/tailLib.js
+++ src/tailLib.js
@@ -4,5 +4,11 @@ const getFormattedLines = function(lines) {
 const getExtractedLines = function(fileContents) {
   return fileContents.slice(fileContents.length - 10);
 };
+const getFileContent = function(fs, path) {
+  if (!fs.exists(path)) {
+    throw new Error(`tail: ${path}: No such file or directory`);
+  }
+  return fs.readFile(path, 'utf8');
+};
 
-module.exports = { getFormattedLines, getExtractedLines };
+module.exports = { getFormattedLines, getExtractedLines, getFileContent };
--- test/testTailLib.js
+++ test/testTailLib.js
@@ -1,5 +1,9 @@
 const assert = require('assert');
-const { getFormattedLines, getExtractedLines } = require('../src/tailLib');
+const {
+  getFormattedLines,
+  getExtractedLines,
+  getFileContent
+} = require('../src/tailLib');
 
 describe('tailLib', () => {
   describe('getFormattedLines', () => {
@@ -10,6 +14,7 @@ describe('tailLib', () => {
       assert.deepStrictEqual(actual, expected);
     });
   });
+
   describe('getExtractedLines', () => {
     it('Should extract lines when the line count greater than 10', () => {
       const content = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
@@ -22,6 +27,23 @@ describe('tailLib', () => {
       const actual = getExtractedLines(content);
       const expected = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
       assert.deepStrictEqual(actual, expected);
+    });
+  });
+
+  describe('getFileContents', () => {
+    it('Should give the file contents when the file exist', () => {
+      const readFile = function(path, encoding) {
+        assert.strictEqual(path, 'a.txt');
+        assert.strictEqual(encoding, 'utf8');
+        return '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11';
+      };
+      const exists = path => {
+        assert.strictEqual(path, 'a.txt');
+        return true;
+      };
+      const expected = '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11';
+      const actual = getFileContent({ readFile, exists }, 'a.txt');
+      assert.strictEqual(actual, expected);
     });
   });
 });
