2b142adee4b4270fb142cb4326420b946cc7593a
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2019-12-23 10:26:10 +0530

    Connected main with functions

--- tail.js
+++ tail.js
@@ -1,3 +1,9 @@
-const main = function() {};
+const { readFileSync, existsSync } = require('fs');
+const { manageUsrArgsAndGiveTail } = require('./src/manageUserArgs');
+
+const main = function() {
+  const fs = { readFile: readFileSync, exists: existsSync };
+  console.log(manageUsrArgsAndGiveTail(process.argv, fs));
+};
 
 main();
