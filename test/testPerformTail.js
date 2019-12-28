b92211814028375c3e3716110e59f151adc089fd
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2019-12-28 10:26:31 +0530

    Added sinon as dev dependency
    * Changed the test cases with sinon

--- test/testPerformTail.js
+++ test/testPerformTail.js
@@ -1,42 +1,34 @@
 'use strict';
 const { assert } = require('chai');
+const { fake, stub } = require('sinon');
 const { performTail } = require('./../src/performTail');
 describe('performTail', () => {
   describe('performTail', () => {
     it('Should give tail 10 lines for one file without options and file contains more than 10 lines ', () => {
+      const readFileSync = stub();
       const cmdLineArgs = ['node', 'tail.js', 'a.txt'];
-      const readFileSync = function(path, encoding) {
-        assert.strictEqual(path, 'a.txt');
-        assert.strictEqual(encoding, 'utf8');
-        return '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n';
-      };
-      const existsSync = path => {
-        assert.strictEqual(path, 'a.txt');
-        return true;
-      };
+      readFileSync
+        .withArgs('a.txt', 'utf8')
+        .returns('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n');
+      const existsSync = stub()
+        .withArgs('a.txt')
+        .returns(true);
       const fs = { readFileSync, existsSync };
       const actual = performTail(cmdLineArgs, fs);
       const expected = { err: '', content: '2\n3\n4\n5\n6\n7\n8\n9\n10\n11' };
       assert.deepStrictEqual(actual, expected);
     });
     it('Should give tail lines for one file without options and file contains less than 10 lines ', () => {
-      const cmdLineArgs = ['node', 'tail.js', 'a.txt'];
-      const readFileSync = function(path, encoding) {
-        assert.strictEqual(path, 'a.txt');
-        assert.strictEqual(encoding, 'utf8');
-        return '9\n10\n11\n';
-      };
-      const existsSync = path => {
-        assert.strictEqual(path, 'a.txt');
-        return true;
-      };
+      const cmdLineArgs = ['node', 'tail.js', 'b.txt'];
+      const readFileSync = fake.returns('9\n10\n11\n');
+      const existsSync = fake.returns(true);
       const fs = { readFileSync, existsSync };
       const actual = performTail(cmdLineArgs, fs);
       const expected = { err: '', content: '9\n10\n11' };
       assert.deepStrictEqual(actual, expected);
     });
     it('Should give error message when the file does not exist', () => {
-      const existsSync = path => false;
+      const existsSync = fake.returns(false);
       const cmdLineArgs = ['node', 'tail.js', 'badFile'];
       const actual = performTail(cmdLineArgs, { existsSync });
       const expected = {
@@ -46,9 +38,8 @@ describe('performTail', () => {
       assert.deepStrictEqual(actual, expected);
     });
     it('Should give the extracted lines when the line count is specified and valid', () => {
-      const readFileSync = (path, encoding) =>
-        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n';
-      const existsSync = path => true;
+      const readFileSync = fake.returns('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n');
+      const existsSync = fake.returns(true);
       const fs = { readFileSync, existsSync };
       const cmdLineArgs = ['node', 'tail.js', '-n', '3', 'tail.js'];
       const actual = performTail(cmdLineArgs, fs);
--- test/testTailLib.js
+++ test/testTailLib.js
@@ -1,5 +1,6 @@
 'use strict';
 const { assert } = require('chai');
+const { fake, stub } = require('sinon');
 const {
   getExtractedLines,
   getFileContent,
@@ -42,21 +43,18 @@ describe('tailLib', () => {
 
   describe('getFileContents', () => {
     it('Should give the file contents when the file exist', () => {
-      const readFileSync = function(path, encoding) {
-        assert.strictEqual(path, 'a.txt');
-        assert.strictEqual(encoding, 'utf8');
-        return '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n';
-      };
-      const existsSync = path => {
-        assert.strictEqual(path, 'a.txt');
-        return true;
-      };
+      const readFileSync = stub()
+        .withArgs('a.txt', 'utf8')
+        .returns('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n');
+      const existsSync = stub()
+        .withArgs('a.txt')
+        .returns(true);
       const expected = { fileContent: '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n' };
       const actual = getFileContent({ readFileSync, existsSync }, 'a.txt');
       assert.deepStrictEqual(actual, expected);
     });
     it('Should give error message for not existing file', () => {
-      const existsSync = path => false;
+      const existsSync = fake.returns(false);
       const expected = { fileErr: 'tail: badFile: No such file or directory' };
       assert.deepStrictEqual(
         getFileContent({ existsSync }, 'badFile'),
